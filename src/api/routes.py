dbcf44d761292bf046461ed170e5d1318d7a487a
Merilola <mariahef12@hotmail.com>
2023-04-20 20:13:14 +0000

    maria

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,9 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import create_access_token
+
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +19,36 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def singup():
+  username = request.json.get("username", None)
+  password = request.json.get("password", None)
+  user = User()
+  user.email = username
+  user.password = password
+  user.is_active = True
+  db.session.add(user)
+  db.session.commit()
+  return "todo esta bien bro",200
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    # Consulta la base de datos por el nombre de usuario y la contraseña
+    user = User.filter.query(username=username, password=password).first()
+    if  User is None:
+          # el usuario no se encontró en la base de datos
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    # crea un nuevo token con el id de usuario dentro
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
+
+
+@api.route('/private', methods=['GET'])
+@jwt_required()
+def private():
+    pass
